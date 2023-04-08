da6a16fdc038f6c35b104e96a9473f51d06bca45
Alex <akorittko@users.noreply.github.com>
2023-04-08 12:31:57 -0400

    ak

--- example_terminal/lib/config.dart
+++ example_terminal/lib/config.dart
@@ -3,5 +3,5 @@ import 'package:flutter/foundation.dart';
 // If you are using a real device to test the integration replace this url
 // with the endpoint of your test server (it usually should be the IP of your computer)
 final kApiUrl = defaultTargetPlatform == TargetPlatform.android || true
-    ? 'https://example-terminal-backend-xq2w.onrender.com'
-    : 'http://localhost:4242';
+    ? 'https://akdyyz.herokuapp.com'
+    : 'https://akdyyz.herokuapp.com';
